d49f2e5890fd2f27029c8ef59dfcdcb0682ef577
calyn05 <67522453+calyn05@users.noreply.github.com>
2022-09-15 14:19:18 +0300

    under construction mode

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
 import "./style.css";
+import imgUrl from "./assets/images/calin.codes-under-construction.jpg";
+
+const background = document.querySelector("body");
+background.style.backgroundImage = `url(${imgUrl})`;
